aabe2c91033f38f7910ec706d171a2947e9c3e59
pastUnknownUser <evan6509@gmail.com>
2024-07-14 18:56:39 -0500

    Put back testing code

--- src/main.cpp
+++ src/main.cpp
@@ -96,6 +96,12 @@ void autonomous() {
 void opcontrol() {
 	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
+    chassis.moveToPoint(10, 10, 1000);
+    pros::delay(1000);
+    chassis.moveToPoint(0, 0, 1000);
+
+    /*
+
 	while (true) {
         
 		 // get left y and right y positions
@@ -110,6 +116,7 @@ void opcontrol() {
         // delay to save resources
         pros::delay(25);
 	}
+    */
 
     
 }
